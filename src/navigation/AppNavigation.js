2681d586be479eaee6bcf66858fe6bbee437fd02
Vladyslav Honchenko <vladyslav.honchenko@rubygarage.org>
2020-03-08 19:45:29 +0200

    change indentions

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -28,138 +28,128 @@ const config = {
   }
 };
 
-const AirPollutionTabNavigator = createMaterialTopTabNavigator(
-  {
-    Carbon: {
-      screen: CarbonDataScreen,
-      navigationOptions: {
-        tabBarLabel: 'CO',
-        tabBarIcon: ({ tintColor }) => (
-          <Ionicons name="ios-settings" color={tintColor} size={24} />
-        )
-      }
-    },
-    Nitrogen: {
-      screen: NitrogenDioxideDataScreen,
-      navigationOptions: {
-        tabBarLabel: 'NO2',
-        tabBarIcon: ({ tintColor }) => (
-          <Ionicons name="ios-settings" color={tintColor} size={24} />
-        )
-      }
-    },
-    Ozone: {
-      screen: OzoneDataScreen,
-      navigationOptions: {
-        tabBarLabel: 'O3',
-        tabBarIcon: ({ tintColor }) => (
-          <Ionicons name="ios-settings" color={tintColor} size={24} />
-        )
-      }
-    },
-    Sulfur: {
-      screen: SulfurDioxideDataScreen,
-      navigationOptions: {
-        tabBarLabel: 'SO2',
-        tabBarIcon: ({ tintColor }) => (
-          <Ionicons name="ios-settings" color={tintColor} size={24} />
-        )
-      }
+const AirPollutionTabNavigator = createMaterialTopTabNavigator({
+  Carbon: {
+    screen: CarbonDataScreen,
+    navigationOptions: {
+      tabBarLabel: 'CO',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-settings" color={tintColor} size={24} />
+      )
     }
   },
-  {
-    initialRouteName: 'Carbon',
-    tabBarPosition: 'bottom',
-    swipeEnabled: true,
-    animationEnabled: true,
-    tabBarOptions: {
-      activeTintColor: 'orange',
-      inactiveTintColor: 'grey',
-      style: {
-        backgroundColor: 'black',
-        borderTopWidth: 0.5,
-        borderTopColor: 'grey'
-      },
-      indicatorStyle: {
-        height: 0
-      },
-      showIcon: true
+  Nitrogen: {
+    screen: NitrogenDioxideDataScreen,
+    navigationOptions: {
+      tabBarLabel: 'NO2',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  },
+  Ozone: {
+    screen: OzoneDataScreen,
+    navigationOptions: {
+      tabBarLabel: 'O3',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  },
+  Sulfur: {
+    screen: SulfurDioxideDataScreen,
+    navigationOptions: {
+      tabBarLabel: 'SO2',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-settings" color={tintColor} size={24} />
+      )
     }
   }
-);
-
-const WeatherNavigator = createStackNavigator(
-  {
-    Main: MainScreen,
-    WeatherWeek: WeatherForWeekScreen,
-    WeatherCurrentDay: WeatherForCurrentDayScreen
-  },
-  config
-);
-
-const WebWeatherNavigation = createStackNavigator(
-  {
-    WebWeather: WebWeatherScreen
-  },
-  config
-);
-
-const WeeklyWeatherNavigation = createStackNavigator(
-  {
-    WeatherWeek: WeatherForWeekScreen,
-    WeatherCurrentDay: WeatherForCurrentDayScreen
-  },
-  config
-);
-
-const MainAppNavigator = createDrawerNavigator(
-  {
-    Weather: {
-      screen: WeatherNavigator,
-      navigationOptions: {
-        drawerLabel: 'Main Page',
-        drawerIcon: ({ tintColor }) => (
-          <Ionicons color={tintColor} name="ios-home" size={20} />
-        )
-      }
+},
+{
+  initialRouteName: 'Carbon',
+  tabBarPosition: 'bottom',
+  swipeEnabled: true,
+  animationEnabled: true,
+  tabBarOptions: {
+    activeTintColor: 'orange',
+    inactiveTintColor: 'grey',
+    style: {
+      backgroundColor: 'black',
+      borderTopWidth: 0.5,
+      borderTopColor: 'grey'
     },
-    WeeklyWeather: {
-      screen: WeeklyWeatherNavigation,
-      navigationOptions: {
-        drawerLabel: 'Weekly Weather',
-        drawerIcon: ({ tintColor }) => (
-          <MaterialCommunityIcons color={tintColor} name="weather-cloudy" size={20} />
-        )
-      }
+    indicatorStyle: {
+      height: 0
     },
-    Air: {
-      screen: AirPollutionTabNavigator,
-      navigationOptions: {
-        drawerLabel: 'Air Pollution',
-        drawerIcon: ({ tintColor }) => (
-          <Ionicons color={tintColor} name="ios-settings" size={20} />
-        )
-      }
-    },
-    WebWeather: {
-      screen: WebWeatherNavigation,
-      navigationOptions: {
-        drawerLabel: 'Weather Map',
-        drawerIcon: ({ tintColor }) => (
-          <Ionicons color={tintColor} name="md-map" size={20} />
-        )
-      }
+    showIcon: true
+  }
+});
+
+const WeatherNavigator = createStackNavigator({
+  Main: MainScreen,
+  WeatherWeek: WeatherForWeekScreen,
+  WeatherCurrentDay: WeatherForCurrentDayScreen
+},
+config);
+
+const WebWeatherNavigation = createStackNavigator({
+  WebWeather: WebWeatherScreen
+},
+config);
+
+const WeeklyWeatherNavigation = createStackNavigator({
+  WeatherWeek: WeatherForWeekScreen,
+  WeatherCurrentDay: WeatherForCurrentDayScreen
+},
+config);
+
+const MainAppNavigator = createDrawerNavigator({
+  Weather: {
+    screen: WeatherNavigator,
+    navigationOptions: {
+      drawerLabel: 'Main Page',
+      drawerIcon: ({ tintColor }) => (
+        <Ionicons color={tintColor} name="ios-home" size={20} />
+      )
     }
   },
-  {
-    drawerBackgroundColor: '#E5E5E5',
-    contentOptions: {
-      activeTintColor: '#cf9b0c',
-      labelStyle: {
-        fontFamily: 'simonetta-bold'
-      }
+  WeeklyWeather: {
+    screen: WeeklyWeatherNavigation,
+    navigationOptions: {
+      drawerLabel: 'Weekly Weather',
+      drawerIcon: ({ tintColor }) => (
+        <MaterialCommunityIcons color={tintColor} name="weather-cloudy" size={20} />
+      )
+    }
+  },
+  Air: {
+    screen: AirPollutionTabNavigator,
+    navigationOptions: {
+      drawerLabel: 'Air Pollution',
+      drawerIcon: ({ tintColor }) => (
+        <Ionicons color={tintColor} name="ios-settings" size={20} />
+      )
+    }
+  },
+  WebWeather: {
+    screen: WebWeatherNavigation,
+    navigationOptions: {
+      drawerLabel: 'Weather Map',
+      drawerIcon: ({ tintColor }) => (
+        <Ionicons color={tintColor} name="md-map" size={20} />
+      )
     }
   }
-);
+},
+{
+  drawerBackgroundColor: '#E5E5E5',
+  contentOptions: {
+    activeTintColor: '#cf9b0c',
+    labelStyle: {
+      fontFamily: 'simonetta-bold'
+    }
+  }
+});
 
 export const AppNavigation = createAppContainer(MainAppNavigator);
